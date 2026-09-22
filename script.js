0ab916b801323459d5c8708a46c10faf8e7faf5b
Kashaf25119 <kashafakhlaq25@gmail.com>
2026-09-22 18:53:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const contactForm =
+    document.getElementById("contactForm");
+
+
+contactForm.addEventListener(
+    "submit",
+    function (event) {
+
+        event.preventDefault();
+
+        const name =
+            document.getElementById("name").value;
+
+        alert(
+            `Thank you, ${name}! Your message has been received.`
+        );
+
+        contactForm.reset();
+
+    }
+);
